96b617d1902eac0fb84ec111ba9fcc614b105434
amiraabouhadid <amiraabouhadid@gmail.com>
2021-06-23 21:51:57 +0200

    remove unnecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
+  entry: './src/index.js',
   devServer: {
     contentBase: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+
   mode: 'development',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
     publicPath: '/',
   },
 
