a2e6f462b3b51d1af43ddd3c26287dca3e7f3d11
amira <amiraabouhadid@gmail.com>
2021-06-24 00:18:05 +0200

    style nav bar

--- src/nav.js
+++ src/nav.js
@@ -2,12 +2,12 @@
 const nav= ()=> {
   const navArr = ['Home', 'Menu', 'Contact'];
   const nav = document.createElement('nav');
-  nav.className = 'nav bg-dark d-flex justify-content-around p-3'
+  nav.classList = 'nav bg-dark d-flex justify-content-around p-3'
 
 
   navArr.forEach((l, i) => {
     const a= document.createElement('a');
-    a.className= 'nav-link text-light';
+    a.classList= 'nav-link text-light';
     a.href= `#${l.toLowerCase()}`;
     a.innerHTML = l;
     nav.append(a);
